ee87c917680f93ec3241b493848a1a35b45c4afe
jayasenapm <pmjayasena123456@gmail.com>
2018-10-19 15:35:29 +0530

    Update web.php

--- web.php
+++ web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-JAYASENA palihakoon
+JAYASENA palihakoon mudi
 Route::get('/', function () {
     return view('welcome');
 });
